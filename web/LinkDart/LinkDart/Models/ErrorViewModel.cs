e4df400e23c088a5dab826fda822795d0021ae18
dhirajdusane <dhirajdusane@gmail.com>
2024-02-24 17:07:13 +0530

    Initial

---
+++ web/LinkDart/LinkDart/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LinkDart.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
